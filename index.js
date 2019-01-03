492734509a29247047d101465ab6be1e4323ec3c
KarthickPrasath Mande <KarthickPrasath.Mande@siriuscom.com>
2019-01-03 19:29:45 +0530

    test

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser') const request = require('request')
-const apiai = require('apiai');
-const app = express()
+let express = require('express')
+let bodyParser = require('body-parser') const request = require('request')
+let apiai = require('apiai');
+let app = express()
 app.set('port', (process.env.PORT || 5001)) app.use(bodyParser.urlencoded({
   extended: false
 }))
 app.use(bodyParser.json()) app.get('/', function(req, res) {
   res.send('Hello world')
 })
-// app.get('/webhook/', function(req, res) {
-//   if (req.query['hub.verify_token'] === "facebookdemoechatbot") {
-//     res.send(req.query['hub.challenge'])
-//   }
-//   res.send('Error, no entry')
-// })
+app.get('/webhook/', function(req, res) {
+  if (req.query['hub.verify_token'] === "facebookdemoechatbot") {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, no entry')
+})
 app.listen(app.get('port'), function() {
   console.log('running on port', app.get('port'))
 })
