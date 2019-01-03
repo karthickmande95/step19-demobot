aa12c5668785305812332a4a428714c2d3be8403
KarthickPrasath Mande <KarthickPrasath.Mande@siriuscom.com>
2019-01-03 19:25:56 +0530

    test

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.set('port', (process.env.PORT || 5001)) app.use(bodyParser.urlencoded({
 app.use(bodyParser.json()) app.get('/', function(req, res) {
   res.send('Hello world')
 })
-app.get('/webhook/', function(req, res) {
-  if (req.query['hub.verify_token'] === "facebookdemoechatbot") {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send('Error, no entry')
-})
+// app.get('/webhook/', function(req, res) {
+//   if (req.query['hub.verify_token'] === "facebookdemoechatbot") {
+//     res.send(req.query['hub.challenge'])
+//   }
+//   res.send('Error, no entry')
+// })
 app.listen(app.get('port'), function() {
   console.log('running on port', app.get('port'))
 })
