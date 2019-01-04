6d3c6cde736256b1757c6078190a36a572316c92
KarthickPrasath Mande <KarthickPrasath.Mande@siriuscom.com>
2019-01-04 16:01:42 +0530

    step4-index.js

--- index.js
+++ index.js
@@ -13,33 +13,6 @@ app.use(bodyParser.json())
 app.get('/', function(req, res) {
   res.send('Hello world')
 })
-// app.post('/webhook', function(req, res) {
-//   var messaging_events = req.body.entry[0].messaging;
-//   for (var i = 0; i < messaging_events.length; i++) {
-//     event = req.body.entry[0].messaging[i];
-//     var senderID = event.sender.id;
-//     if (event.message && event.message.text) {
-//       var messageText = event.message.text;
-//       console.log("senderID" + senderID + "messageText" + messageText);
-//       // var apiaiClientAccessToken = "YOUR_DIALOG_FLOW_CLENT_ACCESS_TOKEN";
-//       var apiaiClientAccessToken = "b93b7f46074e441fbb585c993c2ec1f7";
-//       var app = apiai(apiaiClientAccessToken);
-//       var responseJSON = {};
-//       var request = app.textRequest(messageText, {
-//         sessionId: "sessionId"
-//       });
-//       request.on('response', function(response) {
-//         messageText = response.result.fulfillment.speech;
-//         sendTextMessage(senderID, messageText);
-//       });
-//       request.on('error', function(error) {
-//         sendTextMessage(senderID, messageText);
-//       });
-//       request.end();
-//     }
-//   }
-//   res.sendStatus(200);
-// });
 
 app.post('/webhook', function(req, res) {
   var messaging_events = req.body.entry[0].messaging;
@@ -57,7 +30,6 @@ app.post('/webhook', function(req, res) {
         default:
           console.log("senderID" + senderID + "messageText" + messageText);
           // var apiaiClientAccessToken = "YOUR_DIALOG_FLOW_CLENT_ACCESS_TOKEN";
-          var apiaiClientAccessToken = "b93b7f46074e441fbb585c993c2ec1f7";
           var app = apiai(apiaiClientAccessToken);
           var responseJSON = {};
           var request = app.textRequest(messageText, {
